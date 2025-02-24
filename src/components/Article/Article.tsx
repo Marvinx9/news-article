0a5952032dbff7cbb1cb091793e4dcbc9f3f1d32
Afranio Nunes Dantas <afranio.dantas@livsaude.com.br>
2025-02-24 10:21:28 -0300

    refactor: styles of color to app

--- src/components/Article/Article.tsx
+++ src/components/Article/Article.tsx
@@ -1,24 +1,30 @@
-import IArticle  from "../../Interfaces/IArticle"
+import IArticle from "../../Interfaces/IArticle";
 
-const Article = ({title, text, tags, image, alt} : IArticle) => {
-    return (
-        <div className="news-card">
-            <h3 className="text-xl text-stone-900 font-bold">{title}</h3>
-            <div className="w-full flex-row justify-end gap-2 pr-5 hidden sm:flex">
-                {
-                    tags.map(tag => <span key={tag} className="bg-gray-100 dark:bg-stone-300 px-4 py-1 rounded-full text-xs uppercase hover:scale-110 hover:bg-gradient-to-r hover:from-stone-700">{tag}</span>)
-                }
-            </div>
-            <div className="grid gap-1">
-                {
-                    text.map((content, index) => <p key={index} className="dark:bg-[#A64D79]">{content}</p>)
-                }
-            </div>
-            {image && <img className="sm:p-4 rounded-xl" src={image}/>}
-            {image && alt && <span className="sr-only">{alt}</span>}
-        </div>
-    )
-}
+const Article = ({ title, text, tags, image, alt }: IArticle) => {
+  return (
+    <div className="news-card">
+      <h3 className="text-2xl font-bold">{title}</h3>
+      <div className="w-full flex-row justify-end gap-2 pr-5 hidden sm:flex">
+        {tags.map((tag) => (
+          <span
+            key={tag}
+            className="dark:text-gray-200 bg-gray-200 dark:bg-[#252836] px-4 py-1 rounded-full text-xs uppercase
+            hover:scale-110 transition-all duration-300 ease-in-out 
+            hover:bg-gradient-to-r dark:hover:from-[#3b82f6] dark:hover:to-[#1e293b] hover:to-gray-400"
+          >
+            {tag}
+          </span>
+        ))}
+      </div>
+      <div className="grid gap-2 text-gray-800 dark:text-gray-200">
+        {text.map((content, index) => (
+          <p key={index}>{content}</p>
+        ))}
+      </div>
+      {image && <img className="sm:p-4 rounded-xl" src={image} />}
+      {image && alt && <span className="sr-only">{alt}</span>}
+    </div>
+  );
+};
 
-
-export default Article
+export default Article;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,19 @@
-import { IUser } from '../../Interfaces/IUser'
-import ToggleTheme from '../ToggleTheme/ToggleTheme'
+import { IUser } from "../../Interfaces/IUser";
+import ToggleTheme from "../ToggleTheme/ToggleTheme";
 
-const Header = (props: IUser ) => {
-    return (
-        <div className="flex h-20 bg-gray-400 dark:bg-[#6A1E55] justify-between items-center px-5 sm:rounded-xl sm:m-5">
-            <span className="text-gray-100">
-                Olá, {props.name && props.name !== null ? props.name : 'usuário'}
-            </span>
-            <h1>News Article</h1>
-            <ToggleTheme/>
-        </div>
-    )
-}
+const Header = (props: IUser) => {
+  return (
+    <div
+      className="flex h-20 bg-gray-200 dark:bg-[#121212] text-gray-900 dark:text-gray-200 
+      justify-between items-center px-5 sm:rounded-xl sm:m-5 shadow-md dark:shadow-blue-900/50"
+    >
+      <span>
+        Olá, {props.name && props.name !== null ? props.name : "usuário"}
+      </span>
+      <h1 className="text-lg font-semibold">News Article</h1>
+      <ToggleTheme />
+    </div>
+  );
+};
 
-export default Header
+export default Header;
